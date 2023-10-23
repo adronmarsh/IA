1783b81c1db0aa135823d2ef4bae1b9a818fe145
adronmarsh <adrianmargil8@gmail.com>
2023-10-23 18:58:23 +0200

    update 23/10/2023

--- Big Data Aplicado/scrapingTiendaProductos.py
+++ Big Data Aplicado/scrapingTiendaProductos.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.temu.com/es/electronica-o3-248.html")
+page = requests.get("https://mcdonalds.es/api/sitemap.xml")
 
 soup = BeautifulSoup(page.content)
 # print(soup.prettify)
